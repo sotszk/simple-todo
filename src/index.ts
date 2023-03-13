1c3effff8ad0d38caf292dd70e121277d2a920e2
SotaSuzuki <3.3sota.suzuki@gmail.com>
2023-03-13 20:07:28 +0900

    improve ts catchup

---
+++ src/index.ts
@@ -0,0 +1,13 @@
+import type { Hello } from "./hello";
+import { add } from "@lib/add"
+
+const hello: Hello = 'Hello, world!';
+const myHello: string = hello;
+
+console.log(myHello, add(10));
+
+const helloEl = document.querySelector<HTMLHeadingElement>('.hello');
+
+if (helloEl) {
+  helloEl.textContent = myHello;
+}
