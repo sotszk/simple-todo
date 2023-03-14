e9f05e42060c12aef6142cd60c59f38db50d1b94
SotaSuzuki <3.3sota.suzuki@gmail.com>
2023-03-14 23:02:26 +0900

    improve TodoApp

--- src/App.ts
+++ src/App.ts
@@ -1,11 +1,81 @@
 console.log('App.ts: loaded');
 
+export type TodoItem = {
+  id: number;
+  content: string;
+  createdAt: number;
+  completed: boolean;
+};
+
+export type SubscribableEventType = 'count';
+
 export class App {
-  constructor() {
+  #name: string;
+  #currentId = 1;
+  #subscribedPropEvents: Array<{type: SubscribableEventType; callback: () => void}> = [];
+  items: TodoItem[] = [];
+
+  constructor({name = 'Todo App'}: {name?: string} = {}) {
+    this.#name = name;
+
     console.log('App initialized');
   }
 
-  get name() {
-    return 'todo app';
+  getName() {
+    return this.#name;
+  }
+
+  #incrementId() {
+    this.#currentId++;
+  }
+
+  #createItem(content: string) {
+    const item: TodoItem = {
+      id: this.#currentId,
+      createdAt: Date.now(),
+      content,
+      completed: false,
+    };
+    this.#incrementId();
+    return item;
+  }
+
+  #isSubscribed(eventType: SubscribableEventType) {
+    return this.#subscribedPropEvents.some(event => event.type === eventType);
+  }
+
+  add(content: string) {
+    const newItem = this.#createItem(content);
+    this.items.push(newItem);
+
+    if (this.#isSubscribed('count')) {
+      const {callback} = this.#subscribedPropEvents.find(event => event.type === 'count') ?? {};
+      callback?.();
+    }
+
+    console.log('new item added', newItem);
+  }
+
+  complete(id: number) {
+    this.items = this.items.map(item => (item.id === id ? {...item, complete: true} : item));
+  }
+
+  delete(id: number) {
+    this.items = this.items.filter(item => item.id !== id);
+  }
+
+  getCount() {
+    return this.items.length;
+  }
+
+  subscribe(eventType: 'count', callback: () => void) {
+    if (this.#isSubscribed(eventType)) {
+      return;
+    }
+
+    this.#subscribedPropEvents.push({
+      type: eventType,
+      callback,
+    });
   }
 }
--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,46 @@
 import {App} from './App';
 
 const app = new App();
-console.log(app.name);
+
+console.log('App:', app.getName(), 'instance created');
+
+function init() {
+  const todoCountElement = document.querySelector<HTMLElement>('#js-todo-count');
+  const formElement = document.querySelector<HTMLFormElement>('#js-form');
+
+  if (!todoCountElement || !formElement) {
+    console.log('necessary elements does not found');
+    return;
+  }
+
+  function updateCount() {
+    if (!todoCountElement?.textContent) {
+      return;
+    }
+
+    const textSplitted = todoCountElement.textContent.split(':');
+    textSplitted[1] = ` ${app.getCount()}`;
+    todoCountElement.textContent = textSplitted.join(':');
+  }
+
+  updateCount();
+
+  app.subscribe('count', () => {
+    updateCount();
+  });
+
+  formElement.addEventListener('submit', evt => {
+    evt.preventDefault();
+    const inputItem = formElement.elements[0] as HTMLInputElement;
+    if (!inputItem) {
+      console.log('form item does not found');
+      return;
+    }
+
+    app.add(inputItem.value);
+    inputItem.value = '';
+    inputItem.focus();
+  });
+}
+
+init();
