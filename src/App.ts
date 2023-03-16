7b7da0be6d99e9abf516d3c392fa42eada0c7470
SotaSuzuki <3.3sota.suzuki@gmail.com>
2023-03-16 20:54:21 +0900

    リファクタリング

--- src/App.ts
+++ src/App.ts
@@ -41,6 +41,92 @@ export class App {
     console.log('App initialized');
   }
 
+  mount() {
+    const todoCountElement = document.querySelector<HTMLElement>('#js-todo-count');
+    const formElement = document.querySelector<HTMLFormElement>('#js-form');
+    const todoListElement = document.querySelector<HTMLDivElement>('#js-todo-list');
+
+    if (!todoCountElement || !formElement || !todoListElement) {
+      console.log('necessary elements does not found');
+      return;
+    }
+
+    const updateCount = () => {
+      if (!todoCountElement?.textContent) {
+        return;
+      }
+
+      const textSplitted = todoCountElement.textContent.split(':');
+      textSplitted[1] = ` ${this.getCount()}`;
+      todoCountElement.textContent = textSplitted.join(':');
+    };
+
+    updateCount();
+
+    const updateCountSubscribeEvent: UpdatedSubscribeEvent = {type: 'updated', callback() {
+      updateCount();
+    }};
+
+    const appendTodoElementSubscribeEvent: AddedSubscribeEvent = {type: 'added', callback: item => {
+      const newElement = document.createElement('div');
+      newElement.textContent = item.content;
+      newElement.id = `todo-item-${item.id}`;
+
+      const checkboxElement = document.createElement('input');
+      checkboxElement.setAttribute('type', 'checkbox');
+      checkboxElement.addEventListener('change', evt => {
+        evt.preventDefault();
+        const checked = (evt.target as HTMLInputElement).checked;
+        checkboxElement.checked = checked;
+        if (checked) {
+          this.complete(item.id);
+        } else {
+          this.uncomplete(item.id);
+        }
+      });
+
+      const deleteButtonElement = document.createElement('button');
+      deleteButtonElement.textContent = '削除';
+      deleteButtonElement.setAttribute('type', 'button');
+      deleteButtonElement.addEventListener('click', evt => {
+        evt.preventDefault();
+        this.delete(item.id);
+      });
+
+      newElement.append(checkboxElement);
+      newElement.append(deleteButtonElement);
+      todoListElement.append(newElement);
+    }};
+
+    this.subscribe(updateCountSubscribeEvent);
+    this.subscribe(appendTodoElementSubscribeEvent);
+
+    this.subscribe({type: 'deleted', callback(item) {
+      document.querySelector(`#todo-item-${item.id}`)?.remove();
+    }});
+
+    this.subscribe({type: 'updated', callback: () => {
+      console.log('current items:', this.items);
+    }});
+
+    this.subscribe({type: 'completed', callback(id) {
+      console.log(`item id ${id} completed`);
+    }});
+
+    formElement.addEventListener('submit', evt => {
+      evt.preventDefault();
+      const inputItem = formElement.elements[0] as HTMLInputElement;
+      if (!inputItem) {
+        console.log('form item does not found');
+        return;
+      }
+
+      this.add(inputItem.value);
+      inputItem.value = '';
+      inputItem.focus();
+    });
+  }
+
   getName() {
     return this.#name;
   }
