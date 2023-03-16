a4320cfd1a899f7fb5fca3c229d781e96b7cdc94
SotaSuzuki <3.3sota.suzuki@gmail.com>
2023-03-16 20:22:35 +0900

    とりあえず動くコードを実装

--- src/App.ts
+++ src/App.ts
@@ -7,12 +7,32 @@ export type TodoItem = {
   completed: boolean;
 };
 
-export type SubscribableEventType = 'count';
+export type AddedSubscribeEvent = {type: 'added'; callback: (item: TodoItem) => void};
+export type DeletedSubscribeEvent = {type: 'deleted'; callback: (item: TodoItem) => void};
+export type UpdatedSubscribeEvent = {type: 'updated'; callback: (item: TodoItem) => void};
+export type CompletedSubscribeEvent = {type: 'completed'; callback: (id: number) => void};
+export type SubscribeEvent = AddedSubscribeEvent | DeletedSubscribeEvent | UpdatedSubscribeEvent | CompletedSubscribeEvent;
+
+function isAddedSubscribeEvent(event: SubscribeEvent): event is AddedSubscribeEvent {
+  return event.type === 'added';
+}
+
+function isDeletedSubscribeEvent(event: SubscribeEvent): event is DeletedSubscribeEvent {
+  return event.type === 'deleted';
+}
+
+function isUpdatedSubscribeEvent(event: SubscribeEvent): event is UpdatedSubscribeEvent {
+  return event.type === 'updated';
+}
+
+function isCompletedSubscribeEvent(event: SubscribeEvent): event is CompletedSubscribeEvent {
+  return event.type === 'completed';
+}
 
 export class App {
   #name: string;
   #currentId = 1;
-  #subscribedPropEvents: Array<{type: SubscribableEventType; callback: () => void}> = [];
+  #subscribedEvents: SubscribeEvent[] = [];
   items: TodoItem[] = [];
 
   constructor({name = 'Todo App'}: {name?: string} = {}) {
@@ -40,42 +60,69 @@ export class App {
     return item;
   }
 
-  #isSubscribed(eventType: SubscribableEventType) {
-    return this.#subscribedPropEvents.some(event => event.type === eventType);
-  }
-
   add(content: string) {
     const newItem = this.#createItem(content);
     this.items.push(newItem);
 
-    if (this.#isSubscribed('count')) {
-      const {callback} = this.#subscribedPropEvents.find(event => event.type === 'count') ?? {};
-      callback?.();
+    for (const event of this.#subscribedEvents.filter(event => event.type === 'added')) {
+      if (isAddedSubscribeEvent(event)) {
+        event.callback(newItem);
+      }
+    }
+
+    for (const event of this.#subscribedEvents.filter(event => event.type === 'updated')) {
+      if (isUpdatedSubscribeEvent(event)) {
+        event.callback(newItem);
+      }
     }
 
     console.log('new item added', newItem);
   }
 
   complete(id: number) {
-    this.items = this.items.map(item => (item.id === id ? {...item, complete: true} : item));
+    this.items = this.items.map(item => (item.id === id ? {...item, completed: true} : item));
+
+    for (const event of this.#subscribedEvents.filter(event => event.type === 'completed')) {
+      if (isCompletedSubscribeEvent(event)) {
+        event.callback(id);
+      }
+    }
+  }
+
+  uncomplete(id: number) {
+    this.items = this.items.map(item => (item.id === id ? {...item, completed: false} : item));
   }
 
   delete(id: number) {
+    const target = this.items.find(item => item.id === id);
+    if (!target) {
+      throw new Error('delete target does not found');
+    }
+
     this.items = this.items.filter(item => item.id !== id);
+
+    for (const event of this.#subscribedEvents.filter(event => event.type === 'deleted')) {
+      if (isDeletedSubscribeEvent(event)) {
+        event.callback(target);
+      }
+    }
+
+    for (const event of this.#subscribedEvents.filter(event => event.type === 'updated')) {
+      if (isUpdatedSubscribeEvent(event)) {
+        event.callback(target);
+      }
+    }
   }
 
   getCount() {
     return this.items.length;
   }
 
-  subscribe(eventType: 'count', callback: () => void) {
-    if (this.#isSubscribed(eventType)) {
-      return;
-    }
+  subscribe(event: SubscribeEvent) {
+    this.#subscribedEvents.push(event);
+  }
 
-    this.#subscribedPropEvents.push({
-      type: eventType,
-      callback,
-    });
+  unsubscribe(event: SubscribeEvent) {
+    this.#subscribedEvents = this.#subscribedEvents.filter(evt => evt !== event);
   }
 }
--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,5 @@
 import {App} from './App';
+import type {AddedSubscribeEvent, UpdatedSubscribeEvent} from './App';
 
 const app = new App();
 
@@ -7,8 +8,9 @@ console.log('App:', app.getName(), 'instance created');
 function init() {
   const todoCountElement = document.querySelector<HTMLElement>('#js-todo-count');
   const formElement = document.querySelector<HTMLFormElement>('#js-form');
+  const todoListElement = document.querySelector<HTMLDivElement>('#js-todo-list');
 
-  if (!todoCountElement || !formElement) {
+  if (!todoCountElement || !formElement || !todoListElement) {
     console.log('necessary elements does not found');
     return;
   }
@@ -25,9 +27,55 @@ function init() {
 
   updateCount();
 
-  app.subscribe('count', () => {
+  const updateCountSubscribeEvent: UpdatedSubscribeEvent = {type: 'updated', callback() {
     updateCount();
-  });
+  }};
+
+  const appendTodoElementSubscribeEvent: AddedSubscribeEvent = {type: 'added', callback(item) {
+    const newElement = document.createElement('div');
+    newElement.textContent = item.content;
+    newElement.id = `todo-item-${item.id}`;
+
+    const checkboxElement = document.createElement('input');
+    checkboxElement.setAttribute('type', 'checkbox');
+    checkboxElement.addEventListener('change', evt => {
+      evt.preventDefault();
+      const checked = (evt.target as HTMLInputElement).checked;
+      checkboxElement.checked = checked;
+      if (checked) {
+        app.complete(item.id);
+      } else {
+        app.uncomplete(item.id);
+      }
+    });
+
+    const deleteButtonElement = document.createElement('button');
+    deleteButtonElement.textContent = '削除';
+    deleteButtonElement.setAttribute('type', 'button');
+    deleteButtonElement.addEventListener('click', evt => {
+      evt.preventDefault();
+      app.delete(item.id);
+    });
+
+    newElement.append(checkboxElement);
+    newElement.append(deleteButtonElement);
+    todoListElement.append(newElement);
+  }};
+
+  app.subscribe(updateCountSubscribeEvent);
+  app.subscribe(appendTodoElementSubscribeEvent);
+
+  app.subscribe({type: 'deleted', callback(item) {
+    document.querySelector(`#todo-item-${item.id}`)?.remove();
+  }});
+
+  app.subscribe({type: 'updated', callback() {
+    console.log('current items:', app.items);
+  }});
+
+  app.subscribe({type: 'completed', callback(id) {
+    console.log(`item id ${id} completed`);
+  }});
 
   formElement.addEventListener('submit', evt => {
     evt.preventDefault();
