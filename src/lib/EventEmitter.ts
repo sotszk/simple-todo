8e698865b78d2886bf0393a4e683a5a646b8d167
SotaSuzuki <3.3sota.suzuki@gmail.com>
2023-03-23 17:10:37 +0900

    tweak

--- src/lib/EventEmitter.ts
+++ src/lib/EventEmitter.ts
@@ -1,7 +1,7 @@
 export class EventEmitter<EventNames extends string[] = string[]> {
   #eventListeners = new Set<Map<EventNames[number], () => void>>([]);
 
-  addEventListener(eventName: 'change', callback: () => void) {
+  addEventListener(eventName: EventNames[number], callback: () => void) {
     const eventListener = new Map();
     eventListener.set(eventName, callback);
     this.#eventListeners.add(eventListener);
